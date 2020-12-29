8733819259b9c1113975535342692994aeef07ac
Marcelo Olivas <mfolivas@gmail.com>
2020-12-29 15:11:54 -0500

    adding dependency

--- tests/auth-demo.js
+++ tests/auth-demo.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 
 axios.post('https://service-user-pool-domain-dev-mandalorian-jedi.auth.us-east-1.amazoncognito.com/oauth/token',
-'grant_type=client_credentials&scope=users/profile&client_id=1io956i9130pn1a9dk6kdm0ulh&client_secret=sicvu1vk9r1l1v9vg536p02heaj06i12gl8hmq9ma6qfbcucug7'
+'grant_type=client_credentials&scope=users/profile&client_id=###&client_secret=####'
 )
 .then(function(res) {
     console.log(res);  
